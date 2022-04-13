b66e4cb50750d26ed6aa93a6f299cc90afd8d805
Enzo7121 <enzo.lanus@gmail.com>
2022-04-13 16:02:06 -0300

    background image and sound effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
 import pokeApi from "./api";
 import {
   Center,
@@ -9,12 +10,15 @@ import {
   Button,
   Heading,
   Container,
+  Flex,
   Spinner,
+  Box,
 } from "@chakra-ui/react";
 import JSConfetti from "js-confetti";
 import "./App.css";
 import { Pokemon } from "./types";
-
+import victorySfx from "./sounds/victory.mp3";
+import defeatSfx from "./sounds/defeat.mp3";
 function App() {
   const [pokemon, setPokemon] = useState<Pokemon>({
     image: "",
@@ -27,7 +31,19 @@ function App() {
   const [won, setWon] = useState<boolean>(false);
   const [missed, setMissed] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
+  const [mute, setMute] = useState(true);
   const jsConfetti = new JSConfetti();
+
+  const sfx = {
+    victory: new Howl({
+      src: victorySfx,
+      volume: 0.2,
+    }),
+    defeat: new Howl({
+      src: defeatSfx,
+      volume: 0.2,
+    }),
+  };
 
   const inc = (n: number): number => n + 1;
 
@@ -43,17 +59,20 @@ function App() {
     const formattedName = name.trim().toLowerCase().replace(/\./g, "");
 
     if (formattedName === pokemon.name) {
+      sfx.victory.play();
       setGuessed(inc);
       localStorage.setItem("wins", String(inc(guessed)));
       setWon(true);
       jsConfetti.addConfetti();
       return;
     }
+    sfx.defeat.play();
     setMissed(inc);
     localStorage.setItem("misses", String(inc(missed)));
   };
 
   const handleReset = () => {
+    Howler.stop();
     setAnswered(false);
     setName("");
     setWon(false);
@@ -67,94 +86,102 @@ function App() {
   }, []);
 
   return (
-    <Container>
-      <Center>
-        <Stack
-          width="100%"
-          justifyContent="center"
-          textAlign="center"
-          marginTop={{ base: "2rem", md: "3rem", lg: "3rem" }}
-        >
-          {answered ? (
-            <Stack>
-              <Heading fontSize={{ base: "lg", md: "2xl" }}>
-                The Pokemon is {pokemon.name}
-              </Heading>
-            </Stack>
-          ) : (
-            <Heading fontSize={{ base: "lg", md: "2xl" }}>
-              Who is this Pokemon?
-            </Heading>
-          )}
-          <Stack alignItems="center">
-            {loading ? (
-              <Spinner
-                thickness="4px"
-                speed="0.65s"
-                emptyColor="gray.200"
-                color="blue.500"
-                size="xl"
-              />
-            ) : (
-              <Image
-                boxSize={{ base: "300px", lg: "400px", xl: "450px" }}
-                filter="auto"
-                objectFit="cover"
-                maxWidth={{ base: "400px", md: "600px" }}
-                brightness={answered ? "100%" : "0%"}
-                src={pokemon.image}
-              />
-            )}
-            <Stack direction="column">
-              <Input
-                autoFocus={true}
-                value={name}
-                border="4px"
-                borderColor={answered ? (won ? "green" : "red") : "black"}
-                padding={5}
-                placeholder="Pokemon name..."
-                onChange={(e) => setName(e.target.value)}
-              />
-              <Button
-                border="4px"
-                borderColor="black"
-                backgroundColor="blue.400"
-                color="white"
-                fontWeight="normal"
-                paddingX={10}
-                paddingY={5}
-                isDisabled={loading || answered || !name}
-                onClick={handleAnswer}
-              >
-                Guess
-              </Button>
-            </Stack>
-            <Stack direction="column" alignItems="center" fontSize="xl">
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+      height="100vh"
+      width="100vw"
+    >
+      <Box margin="auto" backgroundColor="white" maxW="500px" rounded="20px">
+        <Center justifyContent="center" alignItems="center">
+          <Stack
+            width="100%"
+            justifyContent="center"
+            textAlign="center"
+            marginTop={{ base: "2rem", md: "3rem", lg: "3rem" }}
+          >
+            {answered ? (
               <Stack>
-                <Text>Wins: {guessed}</Text>
-                <Text>Losses: {missed}</Text>
+                <Heading fontSize={{ base: "lg", md: "xl" }}>
+                  The Pokemon is {pokemon.name}
+                </Heading>
               </Stack>
-              {answered && (
-                <Stack>
-                  <Button
-                    color="white"
-                    border="4px"
-                    borderColor="black"
-                    fontWeight="normal"
-                    paddingX={10}
-                    paddingY={5}
-                    backgroundColor="blue.400"
-                    onClick={handleReset}
-                  >
-                    Volver a jugar
-                  </Button>
-                </Stack>
+            ) : (
+              <Heading fontSize={{ base: "lg", md: "xl" }}>
+                Who is this Pokemon?
+              </Heading>
+            )}
+            <Stack alignItems="center">
+              {loading ? (
+                <Spinner
+                  thickness="4px"
+                  speed="0.65s"
+                  emptyColor="gray.200"
+                  color="blue.500"
+                  size="xl"
+                />
+              ) : (
+                <Image
+                  boxSize={{ base: "300px", lg: "400px", xl: "450px" }}
+                  filter="auto"
+                  objectFit="cover"
+                  maxWidth={{ base: "400px", md: "600px" }}
+                  brightness={answered ? "100%" : "0%"}
+                  src={pokemon.image}
+                />
               )}
+              <Stack direction="column">
+                <Input
+                  autoFocus={true}
+                  value={name}
+                  border="4px"
+                  borderColor={answered ? (won ? "green" : "red") : "black"}
+                  padding={5}
+                  placeholder="Pokemon name..."
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <Button
+                  border="4px"
+                  borderColor="black"
+                  backgroundColor="blue.400"
+                  color="white"
+                  fontWeight="normal"
+                  paddingX={10}
+                  paddingY={5}
+                  isDisabled={loading || answered || !name}
+                  onClick={handleAnswer}
+                >
+                  Guess
+                </Button>
+              </Stack>
+              <Stack direction="column" alignItems="center" fontSize="xl">
+                <Stack>
+                  <Text>Wins: {guessed}</Text>
+                  <Text>Losses: {missed}</Text>
+                </Stack>
+                {answered && (
+                  <Stack>
+                    <Button
+                      color="white"
+                      border="4px"
+                      borderColor="black"
+                      fontWeight="normal"
+                      paddingX={10}
+                      paddingY={5}
+                      backgroundColor="blue.400"
+                      onClick={handleReset}
+                    >
+                      Volver a jugar
+                    </Button>
+                  </Stack>
+                )}
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Center>
-    </Container>
+        </Center>
+      </Box>
+    </Flex>
   );
 }
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,12 @@
-import { extendTheme } from '@chakra-ui/react'
+import {extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
     styles:{
         global: {
-            fontFamily: 'Press Start 2P'
+            fontFamily: 'Press Start 2P',
+            body: {
+                background: "url('https://i.pinimg.com/originals/ca/e0/1a/cae01ab5cce960db0d7819cc96e97ce8.png')",
+            }
         }
     },
     fonts: {
